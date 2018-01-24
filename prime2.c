f9920bb88aa6e4f9837643ecd747a861c151f1a0
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-01-24 16:28:17 +0530

    Create prime2.c

---
+++ prime2.c
@@ -0,0 +1,26 @@
+#inclde<stdio.h>
+int main()
+{
+int low,high,i,flag;
+printf("enter the no');
+scanf("%d%d",&low,&high);
+printf("prime number btween %d and %d are:",low,high);
+while(f<l)
+{
+flag=0;
+for(i=2;i<=low/2;++i)
+{
+if(low%i==0)
+{
+flag=1;
+break;
+}
+}
+if(flag==0)
+{
+printf("%d",low);
+++low;
+}
+retunn 0;
+}
+}
