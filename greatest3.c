98c22bf1512ff720d7167123071ab118dd155008
sankarsanthiya <31505543+sankarsanthiya@users.noreply.github.com>
2018-01-31 11:19:21 +0000

    Create greatest3.c

---
+++ greatest3.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a[10],i,p;
+printf("enter the 3 values:");
+for(i=0;i<3;i++)
+{
+scanf("%d",&a[i]);
+}
+p=a[i];
+if(a[i]>p)
+{
+p=a[i];
+}
+printf("greast num is:%d");
+}
